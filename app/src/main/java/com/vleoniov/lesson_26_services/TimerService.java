b323eedf241de15feb647005529288518ae7cc19
Леонидов Василий Николаевич <Leonidov.V.Ni@sberbank.ru>
2021-08-19 10:32:43 +0300

    Add foreground service.

--- app/src/main/java/com/vleoniov/lesson_26_services/TimerService.java
+++ app/src/main/java/com/vleoniov/lesson_26_services/TimerService.java
@@ -1,12 +1,20 @@
 package com.vleoniov.lesson_26_services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class TimerService extends Service {
 
@@ -15,6 +23,9 @@ public class TimerService extends Service {
     private static final long TIME_COUNTDOWN = 1000 * 600L;
     private static final long TIMER_PERIOD = 1000L;
 
+    private static final String CHANNEL_ID = "channel_id_2";
+    private static final int NOTIFICATION_ID = 1;
+
     public static final String ACTION_CLOSE = "TIMER_SERVICE_ACTION_CLOSE";
 
     private CountDownTimer mCountDownTimer;
@@ -22,6 +33,8 @@ public class TimerService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        createNotificationChannel();
 
         Log.d(TAG, "onCreate() called");
     }
@@ -34,6 +47,8 @@ public class TimerService extends Service {
             stopSelf();
         } else {
             startCountdownTimer(TIME_COUNTDOWN, TIMER_PERIOD);
+
+            startForeground(NOTIFICATION_ID, createNotification(1000));
         }
 
         return START_NOT_STICKY;
@@ -54,11 +69,52 @@ public class TimerService extends Service {
         return null;
     }
 
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.notification_channel_name);
+            String description = getString(R.string.notification_channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private Notification createNotification(long currentTime) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Intent intentCloseService = new Intent(this, TimerService.class);
+        intentCloseService.setAction(ACTION_CLOSE);
+        PendingIntent pendingIntentCloseService = PendingIntent.getService(this, 0, intentCloseService, PendingIntent.FLAG_ONE_SHOT);
+
+        builder.setContentTitle(getString(R.string.timer_service_content_title))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(getString(R.string.timer_service_content_description) + currentTime)
+                .setOnlyAlertOnce(true)
+                .addAction(0, getString(R.string.button_stop_service), pendingIntentCloseService)
+                .setContentIntent(pendingIntent);
+
+        return builder.build();
+    }
+
+    private void updateNotification(@NonNull Notification notification) {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
     private void startCountdownTimer(long time, long period) {
         mCountDownTimer = new CountDownTimer(time, period) {
             @Override
             public void onTick(long millisUntilFinished) {
                 Log.d(TAG, "onTick() called with: millisUntilFinished = [" + millsToSeconds(millisUntilFinished) + "]");
+
+                updateNotification(createNotification(millsToSeconds(millisUntilFinished)));
             }
 
             @Override
